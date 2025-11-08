68fbe9290aa8ad07c23384c27ed179bba60fefff
SirPapkin <piotr8zajac8@gmail.com>
2025-11-08 11:07:33 +0100

    new urdf

--- davie_ws/install/David_assembly_description/share/David_assembly_description/package.zsh
+++ davie_ws/install/David_assembly_description/share/David_assembly_description/package.zsh
@@ -35,8 +35,16 @@ colcon_zsh_convert_to_array() {
 }
 
 # source sh script of this package
-_colcon_package_zsh_source_script "$_colcon_package_zsh_COLCON_CURRENT_PREFIX/share/David_assembly_description/package.sh"
+_colcon_package_zsh_source_script "$_colcon_package_zsh_COLCON_CURRENT_PREFIX/share/david_assembly_description/package.sh"
 unset convert_zsh_to_array
+
+# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced scripts
+COLCON_CURRENT_PREFIX="$_colcon_package_zsh_COLCON_CURRENT_PREFIX"
+
+# source zsh hooks
+_colcon_package_zsh_source_script "$COLCON_CURRENT_PREFIX/share/david_assembly_description/local_setup.zsh"
+
+unset COLCON_CURRENT_PREFIX
 
 unset _colcon_package_zsh_source_script
 unset _colcon_package_zsh_COLCON_CURRENT_PREFIX
