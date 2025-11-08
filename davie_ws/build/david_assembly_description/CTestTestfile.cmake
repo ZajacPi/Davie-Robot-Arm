e924fca0939c1f7beb820422e57e0cc6ee120f6d
zajac <piotr8zajac8@gmail.com>
2025-11-08 11:34:20 +0100

    Cleanup build/install/log from tracking and add .gitignore

--- davie_ws/build/david_assembly_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zajac/Documents/David/Davie-Robot-Arm/davie_ws/src/david_assembly_description
-# Build directory: /home/zajac/Documents/David/Davie-Robot-Arm/davie_ws/build/david_assembly_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
